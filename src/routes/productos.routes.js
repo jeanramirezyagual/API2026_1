54bf08d027f811809103e7c8dfbb7285d036e040
jeanramirezyagual <jean.ramirezyagual4910@upse.edu.ec>
2026-06-17 15:20:46 -0500

    Add image upload middleware to product routes

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,17 +1,14 @@
 import { Router } from 'express';
-
 import {
     getProductos,
     createProducto,
     updateProducto,
     deleteProducto
 } from '../controladores/productosCtrl.js';
-
+import { upload } from '../middlewares/upload.js';
 const router = Router();
-
 router.get('/productos', getProductos);
-router.post('/productos', createProducto);
-router.put('/productos/:id', updateProducto);
+router.post('/productos', upload.single('prod_imagen'), createProducto);
+router.put('/productos/:id', upload.single('prod_imagen'), updateProducto);
 router.delete('/productos/:id', deleteProducto);
-
 export default router;
